135c0218b4963d43d0a609b902d6cf1ff202b3fb
aisyafia <aisya.lutfia@gmail.com>
2022-11-30 10:16:19 +0100

    halfway implementing the delete route, the last one didnt work well

--- routers/storyRouter.js
+++ routers/storyRouter.js
@@ -11,19 +11,32 @@ const router = new Router();
 router.delete("/:id", authMiddleware, async (req, res) => {
   try {
     const userId = req.user.dataValues.id;
-    const storyId = parseInt(req.params.id);
-    const toDelete = await Story.findByPk(storyId, { include: [Space] });
+    const { id } = req.params;
+    const stories = await Story.findAll({
+      where: { spaceId: userId },
+    });
+    // console.log("stories that match", storyId);
 
-    console.log("SPACE USERID", toDelete.space.userId);
-    if (toDelete.space.userId !== userId) {
-      res.status(400).send("Bad request");
-    }
-    if (!toDelete) {
-      res.status(404).send("Story is not found");
-    } else {
-      const deleted = await toDelete.destroy();
-      res.send(deleted);
-    }
+    const storiesNewArray = stories.map((s) => {
+      return s.dataValues.id;
+    });
+    console.log("stories to DELETE", storiesNewArray);
+
+    // const toDelete = await Story.findOne({
+    //   where: { id: storyId.dataValues.id },
+    // });
+    // console.log("STORY to delete", toDelete);
+
+    // console.log("SPACE USERID", toDelete.space.userId);
+    // if (toDelete.space.userId !== userId) {
+    //   res.status(400).send("Bad request");
+    // }
+    // if (!toDelete) {
+    //   res.status(404).send("Story is not found");
+    // } else {
+    //   await toDelete.destroy();
+    //   res.status(200).send([Space]);
+    // }
   } catch (error) {
     console.log("there is an error:", error);
   }
